46921c4af56861f30de1a2f059541abf5a5375af
mmg0311 <mohakgadge@gmail.com>
2019-12-29 00:51:43 +0530

    featured list of students

---
+++ final/final/src/main/java/com/example/generatePDF/PDFforCompany.java
@@ -0,0 +1,84 @@
+package com.example.generatePDF;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.List;
+
+import com.ibm.icu.util.RangeValueIterator.Element;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Font;
+import com.itextpdf.text.FontFactory;
+import com.itextpdf.text.Phrase;
+import com.itextpdf.text.log.LoggerFactory;
+import com.itextpdf.text.pdf.PdfPCell;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfWriter;
+
+import ch.qos.logback.classic.Logger;
+
+public class PDFforCompany {
+//	private static final Logger logger = LoggerFactory.getLogger(GeneratePdfReport.class);
+//
+//    public static ByteArrayInputStream citiesReport(List<City> cities) {
+//
+//        Document document = new Document();
+//        ByteArrayOutputStream out = new ByteArrayOutputStream();
+//
+//        try {
+//
+//            PdfPTable table = new PdfPTable(3);
+//            table.setWidthPercentage(60);
+//            table.setWidths(new int[]{1, 3, 3});
+//
+//            Font headFont = FontFactory.getFont(FontFactory.HELVETICA_BOLD);
+//
+//            PdfPCell hcell;
+//            hcell = new PdfPCell(new Phrase("Id", headFont));
+//            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//            table.addCell(hcell);
+//
+//            hcell = new PdfPCell(new Phrase("Name", headFont));
+//            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//            table.addCell(hcell);
+//
+//            hcell = new PdfPCell(new Phrase("Population", headFont));
+//            hcell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//            table.addCell(hcell);
+//
+//            for (City city : cities) {
+//
+//                PdfPCell cell;
+//
+//                cell = new PdfPCell(new Phrase(city.getId().toString()));
+//                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+//                cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+//                table.addCell(cell);
+//
+//                cell = new PdfPCell(new Phrase(city.getName()));
+//                cell.setPaddingLeft(5);
+//                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+//                cell.setHorizontalAlignment(Element.ALIGN_LEFT);
+//                table.addCell(cell);
+//
+//                cell = new PdfPCell(new Phrase(String.valueOf(city.getPopulation())));
+//                cell.setVerticalAlignment(Element.ALIGN_MIDDLE);
+//                cell.setHorizontalAlignment(Element.ALIGN_RIGHT);
+//                cell.setPaddingRight(5);
+//                table.addCell(cell);
+//            }
+//
+//            PdfWriter.getInstance(document, out);
+//            document.open();
+//            document.add(table);
+//
+//            document.close();
+//
+//        } catch (DocumentException ex) {
+//
+//            logger.error("Error occurred: {0}", ex);
+//        }
+//
+//        return new ByteArrayInputStream(out.toByteArray());
+//    }
+}
