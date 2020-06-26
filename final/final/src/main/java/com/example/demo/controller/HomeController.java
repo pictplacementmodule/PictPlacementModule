11476de49faef801136ff6e44fcb8da58de9bcb7
OmkarDeshpande7 <omideshpande77@gmail.com>
2020-06-26 11:55:33 +0530

    image upload and download in homecontroller added

--- final/final/src/main/java/com/example/demo/controller/HomeController.java
+++ final/final/src/main/java/com/example/demo/controller/HomeController.java
@@ -1,7 +1,12 @@
 package com.example.demo.controller;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -13,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.example.demo.model.Academicdetails;
 import com.example.demo.model.AdminPlaced;
@@ -209,6 +215,7 @@ public class HomeController {
 
 	}
 	
+	
 	@PostMapping("/PlacedByCompany")
 	public void PlacedByCompany(@RequestBody List<String> myParams) {
 		// myParams.size() -1 will contain the company id
@@ -307,4 +314,46 @@ public class HomeController {
 		//alternate solution for return when false
 		return a;
 	}
+	
+	
+	// compress the image bytes before storing it in the database
+	public static byte[] compressBytes(byte[] data) {
+		Deflater deflater = new Deflater();
+		deflater.setInput(data);
+		deflater.finish();
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+		byte[] buffer = new byte[1024];
+		while (!deflater.finished()) {
+			int count = deflater.deflate(buffer);
+			outputStream.write(buffer, 0, count);
+		}
+		try {
+			outputStream.close();
+		} catch (IOException e) {
+		}
+		System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+		return outputStream.toByteArray();
+	}
+	// uncompress the image bytes before returning it to the angular application
+	public static byte[] decompressBytes(byte[] data) {
+		Inflater inflater = new Inflater();
+		inflater.setInput(data);
+		ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+		byte[] buffer = new byte[1024];
+		try {
+			while (!inflater.finished()) {
+				int count = inflater.inflate(buffer);
+				outputStream.write(buffer, 0, count);
+			}
+			outputStream.close();
+		} catch (IOException ioe) {
+		} catch (DataFormatException e) {
+		}
+		return outputStream.toByteArray();
+	}
+}
+	
+	
+	
+	
 }
