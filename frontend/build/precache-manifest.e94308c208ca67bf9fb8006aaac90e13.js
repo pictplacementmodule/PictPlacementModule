ce65ff7e481b3ab8c4b2d2754f2a264bb626f47b
mmg0311 <mohakgadge@gmail.com>
2020-06-30 17:57:41 +0530

    feat: added build files

---
+++ frontend/build/precache-manifest.e94308c208ca67bf9fb8006aaac90e13.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41ddf777fddcd89e373b3826a2f42e17",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf304f4973d429224dfd",
+    "url": "/static/css/2.27d5ada7.chunk.css"
+  },
+  {
+    "revision": "b572f42076101bb84a84",
+    "url": "/static/css/main.35390d42.chunk.css"
+  },
+  {
+    "revision": "cf304f4973d429224dfd",
+    "url": "/static/js/2.5bea5258.chunk.js"
+  },
+  {
+    "revision": "e93fce066aaf6013f83a6e7edc3670f6",
+    "url": "/static/js/2.5bea5258.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b572f42076101bb84a84",
+    "url": "/static/js/main.f4fbf1d5.chunk.js"
+  },
+  {
+    "revision": "f8a6c8d2974cd5d7d100",
+    "url": "/static/js/runtime-main.ca5bd03b.js"
+  }
+]);
