3a646261a29f8cf80efe0e0711fde49184501d02
mmg0311 <mohakgadge@gmail.com>
2020-07-05 16:33:59 +0530

    fix: removed end date for company and renamed branch report

--- frontend/src/containers/Admin/Admin.js
+++ frontend/src/containers/Admin/Admin.js
@@ -27,84 +27,84 @@ import PlaceStudents from "./PlaceStudents";
 import AddCompany from "./Company/AddCompany";
 
 class Admin extends Component {
-  drawerList = {
-    Home: ["", <Home />,'See companies visiting within specific period'],
-    "Add company": ["add-company", <Add />,'Add a new Company from here'],
-    // 'Add Student': ['student-profile', <StudentIcon />],
-    "Add User": ["add-user", <AddUserIcon />,'Create system user (generic)' ],
-    "Edit company": ["edit-company", <EditIcon />,'Edit details of a specific company'], 
-    "Branch Report": ["branch-report", <Receipt />,'Get branchwise details of students'],
-    "Placement Report": ["placement-report", <Receipt />,'number of placed students (company wise)'],
-    "Eligibility Report": ["skills-report", <Receipt />,'company wise report of placed student'],
-    "Pending Request": ["pending-request", <Receipt />,'Students placed by companies. Waiting for confirmation']
-  };
+   drawerList = {
+      Home: ["", <Home />, 'See companies visiting within specific period'],
+      "Add company": ["add-company", <Add />, 'Add a new Company from here'],
+      // 'Add Student': ['student-profile', <StudentIcon />],
+      "Add User": ["add-user", <AddUserIcon />, 'Create system user (generic)'],
+      "Edit company": ["edit-company", <EditIcon />, 'Edit details of a specific company'],
+      "Student List": ["branch-report", <Receipt />, 'Get branchwise details of students'],
+      "Placement Report": ["placement-report", <Receipt />, 'number of placed students (company wise)'],
+      "Eligibility Report": ["skills-report", <Receipt />, 'company wise report of placed student'],
+      "Pending Request": ["pending-request", <Receipt />, 'Students placed by companies. Waiting for confirmation']
+   };
 
-  render() {
-    return (
-      <div>
-        <Route
-          exact
-          path="/admin/company-details/:id"
-          component={CompanyDetails}
-        />
-        <Route exact path="/admin/skill-report/:id" component={StudentReport} />
-        {/* <Route path="/admin/dashboard" component={Dashboard2} /> */}
-        <ProtectedRoute
-          path="/admin/dashboard"
-          component={() => (
-            <Dashboard drawerList={this.drawerList}>
-              <Route
-                exact
-                path="/admin/dashboard/add-company"
-                component={AddCompany}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/add-user"
-                component={AddUser}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/student-profile"
-                component={Profile}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/edit-company"
-                component={EditCompany}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/branch-report"
-                component={BranchReport}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/placement-report"
-                component={CountReport}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/skills-report"
-                component={SkillsReport}
-              />
-              <Route
-                exact
-                path="/admin/dashboard/pending-request"
-                component={PendingRequests}
-              />
-              <Route exact path="/admin/dashboard/" component={AllCompanies} />
-              <Route
-                exact
-                path="/admin/dashboard/place-students"
-                component={PlaceStudents}
-              />
-            </Dashboard>
-          )}
-        />
-      </div>
-    );
-  }
+   render() {
+      return (
+         <div>
+            <Route
+               exact
+               path="/admin/company-details/:id"
+               component={CompanyDetails}
+            />
+            <Route exact path="/admin/skill-report/:id" component={StudentReport} />
+            {/* <Route path="/admin/dashboard" component={Dashboard2} /> */}
+            <ProtectedRoute
+               path="/admin/dashboard"
+               component={() => (
+                  <Dashboard drawerList={this.drawerList}>
+                     <Route
+                        exact
+                        path="/admin/dashboard/add-company"
+                        component={AddCompany}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/add-user"
+                        component={AddUser}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/student-profile"
+                        component={Profile}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/edit-company"
+                        component={EditCompany}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/branch-report"
+                        component={BranchReport}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/placement-report"
+                        component={CountReport}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/skills-report"
+                        component={SkillsReport}
+                     />
+                     <Route
+                        exact
+                        path="/admin/dashboard/pending-request"
+                        component={PendingRequests}
+                     />
+                     <Route exact path="/admin/dashboard/" component={AllCompanies} />
+                     <Route
+                        exact
+                        path="/admin/dashboard/place-students"
+                        component={PlaceStudents}
+                     />
+                  </Dashboard>
+               )}
+            />
+         </div>
+      );
+   }
 }
 
 export default Admin;
